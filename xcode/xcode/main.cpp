b26d965aeb88bda21b8c6912987e73d0d1b65f3b
hidog <hidog.gao@msa.hinet.net>
2013-09-08 21:47:49 +0800

    薪增xcode

---
+++ xcode/xcode/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  xcode
+//
+//  Created by hidog on 13/9/8.
+//  Copyright (c) 2013年 hidog. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+

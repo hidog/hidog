79aefc59bc1c10928eff08d36f249f922572e135
hidog <hidog.gao@msa.hinet.net>
2013-09-22 20:41:54 +0800

    增加分數轉型功能  加法運算 乘法運算等等

--- VS2008/VS2008/VS2008.cpp
+++ VS2008/VS2008/VS2008.cpp
@@ -18,6 +18,36 @@ int _tmain(int argc, _TCHAR* argv[])
 
 	std::cout << f << "\n";
 
+	srand( (int)time(NULL) );
+
+	hidog::numeric::matrix<double>	M(15,15);
+
+	for( int i = 0; i < M.width(); i++ )
+		for( int j = 0; j < M.height(); j++ )
+		{
+			int		r1	=	rand() % 100 - 50;
+			int		r2	=	rand() % 50 + 1;
+
+			M( i, j )	=	1.0 * r1 / r2;
+		}
+
+	hidog::numeric::vector<double>	v(15);
+
+	for( int i = 0; i < v.size(); i++ )
+	{
+		int		r1	=	rand() % 100 - 50;
+		int		r2	=	rand() % 50 + 1;
+
+		v(i)	=	1.0 * r1 / r2;
+	}
+
+	std::cout << v * M * v << "\n";
+
+	hidog::math::Fraction	f1( 352, 85 ),	f2( 24, 47 ),	f3( 21,	3 );
+
+	std::cout << (f1 - f2*f3).toDouble() << std::endl;
+	std::cout << (double)f1 << " " << static_cast<double>(f2) << "\n";
+
 
 	system("PAUSE");
 	return 0;
--- src/math/fraction.h
+++ src/math/fraction.h
@@ -31,17 +31,29 @@ public:
 	Fraction( const Fraction& );
 #endif
 
+	double		toDouble();					// 轉成 double 型態
+	float		toFloat();					// 轉成 float 型態
+
+	Fraction	operator =	( Fraction );
+
+	operator double	();
+
 	friend	std::ostream&	operator << ( std::ostream&, Fraction );
+	friend	Fraction		operator +  ( Fraction, Fraction );
+	friend	Fraction		operator -	( Fraction, Fraction );
+	friend	Fraction		operator *	( Fraction, Fraction );
+
+
 
 private:
-	int		den;				// 分母 denominator
-	int		num;				// 分子 numerator
+	int		den;							// 分母 denominator
+	int		num;							// 分子 numerator
 
 	int		GCD( int, int );
+	int		abs(int);
+	void	ckeck_den_positive();			// 確定分母是正數
 
 };
-
-
 
 
 

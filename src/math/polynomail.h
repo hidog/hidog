e0033f7abeae98cdcc7c278e8df80b78a8356ee1
hidog <hidog.gao@msa.hinet.net>
2013-09-23 10:26:45 +0800

    增加多項式功能

---
+++ src/math/polynomail.h
@@ -0,0 +1,75 @@
+#ifndef	_FRACTION_H
+#define	_FRACTION_H
+
+
+#include "../shared.h"
+#include <iostream>
+
+
+
+
+namespace	hidog
+{
+namespace	math
+{
+
+
+
+
+/*******************************************************************************************
+    fraction obj
+	分數型別
+********************************************************************************************/
+class	Fraction
+{
+public:
+	Fraction();
+	Fraction( int, int );
+	~Fraction();
+	Fraction( Fraction& );
+#if defined(MAC_OS) || defined(LINUX)
+	Fraction( const Fraction& );
+#endif
+
+	double		toDouble();					// 轉成 double 型態
+	float		toFloat();					// 轉成 float 型態
+
+    Fraction	operator =	( Fraction );
+    Fraction    operator += ( Fraction );
+    Fraction    operator -= ( Fraction );
+	Fraction	operator *=	( Fraction );
+	Fraction	operator /=	( Fraction );
+    Fraction    operator -  ();
+    
+
+	operator double	();
+
+	friend	std::ostream&	operator << ( std::ostream&, Fraction );
+	friend	Fraction		operator +  ( Fraction, Fraction );
+	friend	Fraction		operator -	( Fraction, Fraction );
+	friend	Fraction		operator *	( Fraction, Fraction );
+	friend	Fraction		operator /	( Fraction, Fraction );
+
+private:
+	int		den;							// 分母 denominator
+	int		num;							// 分子 numerator
+
+	int		GCD( int, int );
+	int		abs(int);
+	void	ckeck_den_positive();			// 確定分母是正數
+
+};
+
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ function ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+}	// end namespace math
+}	// end namespace hidog
+
+
+
+
+#endif

fa065265f2b917ef9ceecc58b22fe47936561d35
hidog <hidog.gao@msa.hinet.net>
2013-09-24 20:42:37 +0800

    新增complex進來

--- VS2008/VS2008/VS2008.cpp
+++ VS2008/VS2008/VS2008.cpp
@@ -9,6 +9,7 @@
 #include "numeric/matrix.h"
 #include "math/fraction.h"
 #include "math/polynomial.h"
+#include "math/complex.h"
 
 
 
@@ -16,51 +17,9 @@ int _tmain(int argc, _TCHAR* argv[])
 {
 	srand( (int)time(NULL) );
 
-	hidog::numeric::matrix<double>	M,	M2;
-	hidog::numeric::vector<double>	v;
+	hidog::math::Complex	c(12,3.4);
 
-	int		r1,	r2,	r3;
-	int		n,	i,	j;
-
-	for( n = 0; n < 99999999; n++ )
-	{
-		r1	=	rand() % 50 + 30;
-
-		M.resize( r1, r1 );
-		M2.resize( r1, r1 );
-		v.resize( r1 );
-
-		for( i = 0; i < M.width(); i++ )
-			for( j = 0; j < M.height(); j++ )
-			{
-				r1	=	rand() % 200 - 100;
-				r2	=	rand() % 90 + 1;
-
-				M(i,j)	=	1.0 * r1 / r2;
-
-				r1	=	rand() % 200 - 100;
-				r2	=	rand() % 90 + 1;
-
-				M2(i,j)	=	1.0 * r1 / r2;
-			}
-
-		for( i = 0; i < v.size(); i++ )
-		{
-			r1	=	rand() % 200 - 100;
-			r2	=	rand() % 90 + 1;
-
-			v(i)	=	1.0 * r1 / r2;	
-		}
-
-		std::cout << M << "\n";
-		std::cout << v << "\n";
-		std::cout << M*v << "\n";
-		std::cout << v*M*v << "\n";
-		std::cout << M + M2 << "\n";
-		std::cout << M - M2 << "\n";
-		std::cout << M * M2 << "\n";
-
-	}
+	std::cout << c << "\n";
 	
 
 
